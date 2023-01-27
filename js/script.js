59ac54c46a080dcbc8a7bd1967271c9ffafa1b84
Krylova Nataliya <natalinebo2014@yandex.ru>
2023-01-27 11:14:06 +0100

    feat: add sliders functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,47 @@
 
+//--------------- first-screen slide---------
+
+const slider = document.querySelector(".slider");
+const slideBtnsWrapper = document.querySelector(".slide-btns-wrapper");
+const slideBtns = document.querySelectorAll(".slide-btn");
+let countSlide = 0;
+
+slideBtnsWrapper.addEventListener('click', function(event) {
+    if (event.target.classList.contains("slide-btn")) {
+      slideBtns.forEach((btn, index) => {
+        btn.classList.remove("active");
+        event.target.classList.add("active");
+        if (event.target === btn) {
+          countSlide = index;
+          console.log(countSlide);
+          slider.style.transform = `translateX(${-630 * countSlide}px)`;
+        }
+      });
+    }
+});
+
+//--------------- order-slide-----------------
+
+const orderSlider = document.querySelector('.order-slider');
+const orderBtnsWrapper = document.querySelector('.order-btns-wrapper');
+const orderSlideBtns = document.querySelectorAll('.order-slide-btn');
+let count = 0;
+
+orderBtnsWrapper.addEventListener('click', function(event) {
+    if (event.target.classList.contains('order-slide-btn')) {
+        orderSlideBtns.forEach((btn, index) => {
+            btn.classList.remove('active');
+            event.target.classList.add('active');
+            if (event.target === btn) {
+                count = index;
+                console.log(count);
+                orderSlider.style.transform = `translateX(${(-1133) * count}px)`;
+                console.log()
+            }
+        });
+    }
+});
+
 //--------------- small slider ----------------
 const colorSelect = document.querySelector('.color-select');
 const colorChoiceLabel = document.querySelectorAll('.color-choice');
@@ -16,6 +59,7 @@ img.alt = `электрический велосипед ${colorDescription[num 
 
 smallSlide.append(p);
 smallSlide.append(img);
+smallSlide.classList.add('visible');
 
 colorSelect.addEventListener('click', function(event) {
     smallSlide.classList.add('opacity')
