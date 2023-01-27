398b9543076d8cf4e668694df963cbeca75317d3
Krylova Nataliya <natalinebo2014@yandex.ru>
2023-01-27 17:31:39 +0100

    feat: add adaptive styles

--- js/script.js
+++ js/script.js
@@ -1,41 +1,53 @@
+//--------------- burger-menu ---------------
+
+//https://codepen.io/hexagoncircle/pen/dJMERy
 
 //--------------- first-screen slide---------
 
 const slider = document.querySelector(".slider");
+const sliderWrapper = document.querySelector('.slider-wrapper');
 const slideBtnsWrapper = document.querySelector(".slide-btns-wrapper");
 const slideBtns = document.querySelectorAll(".slide-btn");
 let countSlide = 0;
 
+console.log(document.documentElement.clientWidth);
+
 slideBtnsWrapper.addEventListener('click', function(event) {
     if (event.target.classList.contains("slide-btn")) {
+      let translateNum = sliderWrapper.clientWidth;
       slideBtns.forEach((btn, index) => {
-        btn.classList.remove("active");
-        event.target.classList.add("active");
-        if (event.target === btn) {
-          countSlide = index;
-          console.log(countSlide);
-          slider.style.transform = `translateX(${-630 * countSlide}px)`;
-        }
-      });
+          btn.classList.remove("active");
+          event.target.classList.add("active");
+          if (event.target === btn) {
+            countSlide = index;
+            console.log(countSlide);
+            slider.style.transform = `translateX(${-translateNum * countSlide}px)`;
+          }
+        });
     }
 });
 
 //--------------- order-slide-----------------
 
 const orderSlider = document.querySelector('.order-slider');
+const orderSliderWrapper = document.querySelector('.order-slider-wrapper');
 const orderBtnsWrapper = document.querySelector('.order-btns-wrapper');
 const orderSlideBtns = document.querySelectorAll('.order-slide-btn');
 let count = 0;
+let translateNum = orderSliderWrapper.clientWidth;
+console.log(translateNum);
 
 orderBtnsWrapper.addEventListener('click', function(event) {
     if (event.target.classList.contains('order-slide-btn')) {
+        let translateNum = orderSliderWrapper.clientWidth;
+        console.log(translateNum);
         orderSlideBtns.forEach((btn, index) => {
             btn.classList.remove('active');
             event.target.classList.add('active');
             if (event.target === btn) {
                 count = index;
                 console.log(count);
-                orderSlider.style.transform = `translateX(${(-1133) * count}px)`;
+                orderSlider.style.transform = `translateX(${(-translateNum) * count}px)`;
                 console.log()
             }
         });
